e27628d16da0b4d93a608e7a5ea91117ee2d382f
Alexa <alexakearns@outlook.com>
2020-06-22 16:27:09 +0100

    Add print_list to output list array -Red

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -11,4 +11,10 @@ describe Menu do
     subject.add_dish('burger', 6.99)
     expect(subject.list[0].price).to be 6.99
   end
+
+  it 'prints list of dishes with name and price' do
+    subject.add_dish('burger', 6.99)
+    subject.add_dish('pasta', 5)
+    expect { subject.print_list }.to output("Name     Price\nburger    £6.99\npasta    £5\n").to_stdout
+  end
 end
