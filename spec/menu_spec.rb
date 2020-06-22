a161111044bd7db8591670feb4189e291ce4f88c
Alexa <alexakearns@outlook.com>
2020-06-22 12:10:15 +0100

    Menu add_dish list contains price - Green

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,4 +6,9 @@ describe Menu do
     subject.add_dish('burger', 6.99)
     expect(subject.list).to include 'burger'
   end
+
+  it 'list has price of dish included' do
+    subject.add_dish('burger', 6.99)
+    expect(subject.list['burger'].price).to be 6.99
+  end
 end
