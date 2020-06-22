cdfab9bced43ecd9d702f43223a23d690da484a1
Alexa <alexakearns@outlook.com>
2020-06-22 12:08:13 +0100

    Add menu class with add_dish method - Green

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,4 +2,14 @@ require_relative 'dish'
 
 class Menu
 
+  attr_reader :list
+
+  def initialize
+    @list = {}
+  end
+
+  def add_dish(name, price)
+    @list[name] = Dish.new(name, price)
+  end
+
 end
