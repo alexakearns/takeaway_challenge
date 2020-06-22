3b1cc3602ec9e006b245da020ac09b0ec43fa828
Alexa <alexakearns@outlook.com>
2020-06-22 12:18:38 +0100

    Test key to dish is the same as name - Green

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -11,4 +11,9 @@ describe Menu do
     subject.add_dish('burger', 6.99)
     expect(subject.list['burger'].price).to be 6.99
   end
+
+  it '#add_dish stores name as key to dish' do
+    subject.add_dish('burger', 6.99)
+    expect(subject.list).to have_key 'burger'
+  end
 end
