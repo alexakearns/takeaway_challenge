9dca3046e0730f0802533d1fe010f6279b7275e3
Alexa <alexakearns@outlook.com>
2020-06-22 16:27:16 +0100

    Add print_list to output list array - Green

--- lib/menu.rb
+++ lib/menu.rb
@@ -6,10 +6,17 @@ class Menu
   
   def initialize
     @list = []
-    @header = 'Name     Price   '
+    @header = 'Name     Price'
   end
 
   def add_dish(name, price)
     @list << Dish.new(name, price)
+  end
+
+  def print_list
+    puts @header
+    @list.each { |dish| 
+      puts "#{dish.name}    £#{dish.price}" 
+    }
   end
 end
